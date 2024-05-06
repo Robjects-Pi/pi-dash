f726e044a20df4ed8a6ced5dce695c6c65f9c4b1
Mike <garzarobm@gmail.com>
2024-05-05 20:06:18 -0500

    Remove legacy code for uploading files and recording 10 seconds of video

--- software/camera/bullseye-bookworm/python/record-10-secs/main.py
+++ software/camera/bullseye-bookworm/python/record-10-secs/main.py
@@ -1,11 +1,15 @@
 from lib import Camera
 import time
+import configparser
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+duration = config['camera']['duration']
 
 # Record and stop after 10 seconds
 camera = Camera()
 camera.start_recording()
 print("Recording")
-time.sleep(10)
+time.sleep(duration)
 print("Done Recording")
 camera.stop_recording()
